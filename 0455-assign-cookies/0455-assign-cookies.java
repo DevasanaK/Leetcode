8d71ef670540efffdea761d350a43dd76f1e975a
DEVASANA K <118067825+DevasanaK@users.noreply.github.com>
2024-08-07 17:42:57 +0530

    Time: 8 ms (98.35%), Space: 45.2 MB (51.86%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int l=0;
+        int r=0;
+        int n=g.length;
+        int m=s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(l<m && r<n){
+            if(g[r]<=s[l]){
+                r++;
+            }
+            l++;
+        }
+        return r;
+    }
+}
